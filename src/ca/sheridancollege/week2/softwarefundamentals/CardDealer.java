b877211fe563034e46cc8160b8c2863b6052831e
vinit <vinitpatel2803@gmail.com>
2022-02-02 21:35:55 -0500

    object is added

--- src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/CardDealer.java
@@ -9,11 +9,16 @@ package ca.sheridancollege.week2.softwarefundamentals;
  * The class where we create Cards
  * @author dancye, 2019
  */
+
+
 public class CardDealer 
 {
+    
     public static void main(String[] args)
     {
         Card nineClubs = new Card("Clbs", 9);
         nineClubs.setSuit("diamonds");
+        Card c = new Card("hearts",8);
+        c.setSuit("hearts");
     }
 }
