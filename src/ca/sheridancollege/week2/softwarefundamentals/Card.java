56ae96c466b59a9d4d4589a07811737820e9ce98
vinit <vinitpatel2803@gmail.com>
2022-02-02 21:10:26 -0500

    comment added

--- src/ca/sheridancollege/week2/softwarefundamentals/Card.java
+++ src/ca/sheridancollege/week2/softwarefundamentals/Card.java
@@ -9,6 +9,7 @@ package ca.sheridancollege.week2.softwarefundamentals;
  * A class to model Card Objects.
  * Created in Week 2 for the cloning repository demo
  * @author dancye, 2019
+ * name: - patel vinitkumar dipakbhai
  */
 public class Card 
 {
